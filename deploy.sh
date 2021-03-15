ec8463de5f558e3927fa7896ebd30236091aec7e
yue.chang <r3399r@gmail.com>
2021-03-15 16:59:47 +0800

    add s3, cloudfront, route53 script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 env=$1
 project=$2
 
@@ -27,6 +29,11 @@ echo env: $1
 echo project: $2
 echo ==================================================================================
 
+echo deploy infrastructure...
+aws cloudformation package --template-file process-template.yaml --output-template-file packaged.yaml --s3-bucket y-cf-midway
+aws cloudformation deploy --template-file packaged.yaml --stack-name $2-$1-stack --parameter-overrides ProjectName=$2 TargetEnvr=$1
+echo ==================================================================================
+
 echo deploy web to s3...
 cd ../$2
 npm run deploy:$1
@@ -36,8 +43,3 @@ echo deploy lambda...
 cd ../celestial-service
 npm run deploy:$1
 echo ==================================================================================
-
-echo deploy cloudfront...
-cd ../celestial-system
-aws cloudformation package --template-file cloudfront-template.yaml --output-template-file packaged.yaml --s3-bucket y-cf-midway
-aws cloudformation deploy --template-file packaged.yaml --stack-name $2-$1-stack --parameter-overrides ProjectName=$2 TargetEnvr=$1
