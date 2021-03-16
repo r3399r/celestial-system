8b8477cf78aa270a72faadc5c280e9461e2e1a44
yue.chang <r3399r@gmail.com>
2021-03-16 09:53:49 +0800

    apply distribution to api

--- deploy.sh
+++ deploy.sh
@@ -24,22 +24,23 @@ if [ $2 != "sadalsuud" ] &&
     exit 1
 fi
 
-echo ==================================================================================
+echo ====================================================================================
 echo env: $1
 echo project: $2
-echo ==================================================================================
-
-echo deploy infrastructure...
-aws cloudformation package --template-file process-template.yaml --output-template-file packaged.yaml --s3-bucket y-cf-midway
-aws cloudformation deploy --template-file packaged.yaml --stack-name $2-$1-stack --parameter-overrides ProjectName=$2 TargetEnvr=$1
-echo ==================================================================================
-
-echo deploy web to s3...
-cd ../$2
-npm run deploy:$1
-echo ==================================================================================
+echo ====================================================================================
 
 echo deploy lambda...
 cd ../celestial-service
 npm run deploy:$1
-echo ==================================================================================
+echo ====================================================================================
+
+echo deploy infrastructure...
+cd ../celestial-system
+aws cloudformation package --template-file process-template.yaml --output-template-file packaged.yaml --s3-bucket y-cf-midway
+aws cloudformation deploy --template-file packaged.yaml --stack-name $2-$1-stack --parameter-overrides ProjectName=$2 TargetEnvr=$1
+echo ====================================================================================
+
+echo deploy web to s3...
+cd ../$2
+npm run deploy:$1
+echo ====================================================================================
