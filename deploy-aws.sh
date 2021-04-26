6d9e3cafa6b709c0553113afcb2b7d6720db82e5
yue.chang <r3399r@gmail.com>
2021-04-26 10:29:46 +0800

    add tagging process

--- deploy-aws.sh
+++ deploy-aws.sh
@@ -33,8 +33,41 @@ echo env: $1
 echo project: $2
 echo ====================================================================================
 
+if [ $1 = "prod" ]
+  then
+    echo tagging celestial-service...
+    cd ../celestial-service
+    version=$(node -pe "require('./package.json').version")
+    
+    if git diff v$version | grep "^diff";
+      then
+        npm version minor
+
+        git push
+        versionNew=$(node -pe "require('./package.json').version")
+        git push origin v$versionNew
+    fi
+
+    echo ====================================================================================
+    echo tagging $2...
+    cd ../$2
+    version=$(node -pe "require('./package.json').version")
+    
+    if git diff v$version | grep "^diff";
+      then
+        npm version minor
+
+        git push
+        versionNew=$(node -pe "require('./package.json').version")
+        git push origin v$versionNew
+    fi
+    echo ====================================================================================
+fi
+
 echo deploy lambda...
 cd ../celestial-service
+git checkout master
+git pull
 npm run pre:deploy
 aws cloudformation package --template-file aws/cloudformation/template.yaml --output-template-file packaged.yaml --s3-bucket y-cf-midway
 aws cloudformation deploy --template-file packaged.yaml --stack-name celestial-service-$1-stack --parameter-overrides TargetEnvr=$1 --no-fail-on-empty-changeset
@@ -42,12 +75,16 @@ echo ===========================================================================
 
 echo deploy infrastructure...
 cd ../celestial-system
+git checkout master
+git pull
 aws cloudformation package --template-file process-template.yaml --output-template-file packaged.yaml --s3-bucket y-cf-midway
 aws cloudformation deploy --template-file packaged.yaml --stack-name $2-$1-stack --parameter-overrides ProjectName=$2 TargetEnvr=$1 HostName=$host --no-fail-on-empty-changeset
 echo ====================================================================================
 
 echo deploy web to s3...
 cd ../$2
+git checkout master
+git pull
 npm run pre:deploy
 aws s3 sync ./dist s3://$2-$1 --delete --cache-control max-age=0
 echo ====================================================================================
